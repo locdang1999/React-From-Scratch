5a041488bd90732731e8cf45d62747790696ef14
Dang Gia Loc <74612458+locdang1999@users.noreply.github.com>
2023-11-01 08:54:42 +0700

    setup project

---
+++ src/client/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./assets/styles/style.css";
+import { Typography } from './components/Typography';
+
+const App = () => {
+  return (
+    <>
+      <Typography variant='h2'> Hello </Typography>
+    </>
+  )
+}
+
+export default App
